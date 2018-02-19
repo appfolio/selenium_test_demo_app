b123be882372792d91a2873f6a5a922602f7c9af
Natalie Schauser <natalie.schauser@appfolio.com>
2018-02-18 22:10:49 -0800

    ns - fix test_can_create_a_new_blog_post

--- test/page_objects/blog_posts/new_page.rb
+++ test/page_objects/blog_posts/new_page.rb
@@ -1,4 +1,18 @@
 module BlogPosts
   class NewPage < AePageObjects::Document
+    path :new_blog_post
+
+    element :submit_button, locator: '.js-submit'
+
+    form_for :blog_post do
+      element :title
+      element :body
+    end
+
+    def submit!
+      submit_button.node.click
+      stale!
+      window.change_to(ShowPage)
+    end
   end
 end
--- test/page_objects/blog_posts/show_page.rb
+++ test/page_objects/blog_posts/show_page.rb
@@ -1,4 +1,8 @@
 module BlogPosts
   class ShowPage < AePageObjects::Document
+    path :blog_post
+
+    element :title, locator: '.js-title'
+    element :body, locator: '.js-body'
   end
 end
