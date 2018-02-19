a966e4676f573ca6707b495703be5020c0b0acd6
Natalie Schauser <natalie.schauser@appfolio.com>
2018-02-18 21:46:21 -0800

    ns - fix test_blog_posts_appear_on_index_page

--- test/page_objects/blog_posts/index_page.rb
+++ test/page_objects/blog_posts/index_page.rb
@@ -3,5 +3,10 @@ module BlogPosts
     path :blog_posts
 
     element :title, locator: '.js-title'
+
+    collection :blog_posts, locator: 'tbody', item_locator: 'tr' do
+      element :title, locator: '.js-blog-title'
+      element :body, locator: '.js-blog-body'
+    end
   end
 end
