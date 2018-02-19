67074e1b31e9d8eb6a6947f481e5dce4114eb69d
Natalie Schauser <natalie.schauser@appfolio.com>
2018-02-19 10:15:29 -0800

    ns - fix test_can_delete_a_blog_post

--- test/page_objects/blog_posts/index_page.rb
+++ test/page_objects/blog_posts/index_page.rb
@@ -7,6 +7,15 @@ module BlogPosts
     collection :blog_posts, locator: 'tbody', item_locator: 'tr' do
       element :title, locator: '.js-blog-title'
       element :body, locator: '.js-blog-body'
+      element :delete_button, locator: '.js-delete'
+
+      def delete!
+        delete_button.node.click
+        alert = node.base.driver.browser.switch_to.alert
+        yield alert if block_given?
+        alert.accept
+        window.change_to(IndexPage)
+      end
     end
   end
 end
